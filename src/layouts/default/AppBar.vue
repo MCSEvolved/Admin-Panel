a0bfe97a50a504ebc7806355fe2e2a5303d54cd0
IICarst <zwartenkotcarst@gmail.com>
2023-05-30 21:38:10 +0200

    initial commit

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <navbar logoSrc="/emerald-exchange-wide.svg" />
+</template>
+
+<script lang="ts">
+import {applyReactInVue} from 'veaury'
+import Navbar from 'mcs-navbar'
+
+export default {
+  components: {
+    // Use HOC 'applyReactInVue'
+    navbar: applyReactInVue(Navbar)
+  }
+}
+</script>
