16ba359c2de356358ef27146cab6f114ea9a9dd9
IICarst <zwartenkotcarst@gmail.com>
2023-06-05 16:56:55 +0200

    added edit and delete dialogs to update or delete rules

---
+++ src/components/nginx/DeleteRuleDialog.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-dialog
+      v-model="dialog"
+      persistent
+      width="512"
+  >
+    <template v-slot:activator="{ props }">
+      <v-btn
+          color="red"
+          v-bind="props"
+      >
+        DELETE
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">Delete</span>
+      </v-card-title>
+      <v-card-text>
+        Are you sure you want to delete this rule?
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+            color="blue-darken-1"
+            variant="text"
+            @click="dialog = false"
+        >
+          Close
+        </v-btn>
+        <v-btn
+            color="red"
+            variant="text"
+            @click="dialog = false"
+        >
+          DELETE
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+  import {ref} from 'vue'
+  const dialog = ref(false)
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/nginx/EditRuleDialog.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-dialog
+      v-model="dialog"
+      persistent
+      width="1024"
+  >
+    <template v-slot:activator="{ props }">
+      <v-btn
+          color="blue"
+          v-bind="props"
+      >
+        EDIT
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">Edit</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col
+                cols="12"
+                sm="6"
+                md="4"
+            >
+              <v-text-field
+                  label="Service Name"
+                  :model-value="title"
+                  required
+              ></v-text-field>
+            </v-col>
+            <v-col
+                cols="12"
+                sm="6"
+                md="4"
+            >
+              <v-text-field
+                  label="Location"
+                  :model-value="location"
+                  hint="/api/tracker"
+              ></v-text-field>
+            </v-col>
+            <v-col
+                cols="12"
+                sm="6"
+                md="4"
+            >
+              <v-text-field
+                  label="Port"
+                  :model-value="port"
+                  readonly
+                  disabled
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+            color="blue-darken-1"
+            variant="text"
+            @click="dialog = false"
+        >
+          Close
+        </v-btn>
+        <v-btn
+            color="blue-darken-1"
+            variant="text"
+            @click="dialog = false"
+        >
+          Save
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+  import {ref} from 'vue'
+  const dialog = ref(false)
+  const props = defineProps(['item'])
+
+  const title = props.item.columns.title
+  const location = props.item.columns.location
+  const port = props.item.columns.port
+</script>
+
+<style scoped>
+
+</style>
