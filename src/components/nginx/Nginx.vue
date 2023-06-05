f80b4b96541793dffa47b525eb0f597fca2a0ad8
IICarst <zwartenkotcarst@gmail.com>
2023-06-05 16:56:36 +0200

    added data table that shows all the rules

--- src/components/nginx/Nginx.vue
+++ src/components/nginx/Nginx.vue
@@ -1,20 +1,48 @@
 <template>
     <h1>Nginx Control</h1>
 
-    <v-card class="list" color="donkerblauw">
-        <v-data-table
-            :headers="headers"
-            :items="items"
-            item-key="title"
-            items-per-page="5"
-            color="purple"
-            height="100%"
-        ></v-data-table>
-    </v-card>
+    <v-container class="list" color="donkerblauw">
+      <v-layout column style="height: 100%">
+        <v-card md6 style="overflow-y: auto; width: 100%">
+          <v-data-table
+              :headers="headers"
+              :items="items"
+              item-key="title"
+              items-per-page="10"
+              :loading="false"
+          >
+
+            <template v-slot:column.edit="{ item }">
+              Edit
+            </template>
+
+            <template v-slot:column.delete="{ item }">
+              Delete
+            </template>
+
+            <template v-slot:item.edit="{ item }">
+              <EditRuleDialog
+                  :item="item"
+              />
+            </template>
+
+            <template v-slot:item.delete="{ item }">
+              <DeleteRuleDialog />
+            </template>
+
+          </v-data-table>
+        </v-card>
+      </v-layout>
+    </v-container>
+
 </template>
 
 <script setup>
 import {ref} from 'vue'
+import EditRuleDialog from "./EditRuleDialog.vue";
+import DeleteRuleDialog from "./DeleteRuleDialog.vue";
+
+
     const items = ref([
         {
             title: "Filesyncer",
@@ -22,6 +50,14 @@ import {ref} from 'vue'
             port: 8469
         }
     ])
+
+for(let i = 0 ; i < 100 ; i++) {
+  items.value.push({
+    title: `Filesyncer_${i}`,
+    location: "/filesyncer",
+    port: 8469
+  })
+}
 
     const headers = [
         { title: 'Service name', key: 'title', align: 'start'},
@@ -32,7 +68,7 @@ import {ref} from 'vue'
       ]
 </script>
 
-<style scoped>
+<style>
 h1 {
     text-align: center;
     color: white;
@@ -44,4 +80,6 @@ h1 {
     margin-right: auto;
     height: 90%;
 }
+
+
 </style>
