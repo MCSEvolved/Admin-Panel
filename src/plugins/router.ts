be6676d0cbf923bad6b0b2d8ef9585fee8b12949
IICarst <67209089+IICarst@users.noreply.github.com>
2023-06-22 08:41:38 +0200

    Update router.ts

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -4,7 +4,8 @@ import Docker from "../components/docker/Docker.vue"
 import DockerLogs from "../components/docker/DockerLogs.vue"
 
 const routes = [
-    {name: "nginx", path: '/admin-panel/', component: Nginx},
+    {path: '/', redirect: {name: "nginx"}},
+    {path: '/admin-panel/', redirect: {name: "nginx"}},
     {name: "nginx", path: '/admin-panel/nginx', component: Nginx},
     {name: "docker", path: '/admin-panel/docker', component: Docker},
     {name: "docker-logs", path: '/admin-panel/docker/:serviceName/logs', component: DockerLogs}
