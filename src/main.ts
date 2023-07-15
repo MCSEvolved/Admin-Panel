e28a406f06572454f4e5d8db97466be695160ecd
IICarst <zwartenkotcarst@gmail.com>
2023-07-15 19:36:48 +0200

    added auth

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import "./styles/app.css"
 import { vuetify } from './plugins/vuetify'
 import { pinia } from './plugins/pinia'
 import { router } from './plugins/router'
+import { initializeApp } from 'firebase/app'
+
+initializeApp({
+    apiKey: "AIzaSyBlfZjJyhjcgyPfxaqkZHSR5SciFBWC5IY",
+    authDomain: "mcsynergy-55878.firebaseapp.com",
+    projectId: "mcsynergy-55878",
+    storageBucket: "mcsynergy-55878.appspot.com",
+    messagingSenderId: "822930182678",
+    appId: "1:822930182678:web:23e8f0b3e044ae06cb9b37",
+    measurementId: "G-3BK4KLMGTJ"
+})
 
 createApp(App)
     .use(vuetify)
--- src/stores/NginxStore.ts
+++ src/stores/NginxStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import axios, { AxiosError } from 'axios'
+import { getAuth } from "firebase/auth";
 
 export interface NginxRule {
     id: number
@@ -14,18 +15,24 @@ export const useNginxStore = defineStore('nginx', () => {
     const rules = ref<NginxRule[]>([])
 
     const fetchAllRules = async () => {
-        const res = await axios.get<NginxRule[]>("https://api.mcsynergy.nl/admin-panel/nginx/all")
-        .catch((error: AxiosError<{message: string}>) => 
-            alert(`Something went wrong while fetching rules.\n${error.response?.data.message}`)
-        )
+        const res = await axios.get<NginxRule[]>("https://api.mcsynergy.nl/admin-panel/nginx/all", {
+            headers: {Authorization: `Bearer ${getAuth().currentUser?.getIdToken(true)}`}
+        })
+        .catch((error: AxiosError<{message: string}>) => {
+            if(error.response?.data.message === "failed to get claims") window.location.replace("https://mcsynergy.nl/login")
+            else alert(`Something went wrong while fetching rules.\n${error.response?.data.message}`)
+        })
         if(res) rules.value = res.data
     }
 
     const fetchRuleById = async (id: number) => {
-        const res = await axios.get<NginxRule>(`https://api.mcsynergy.nl/admin-panel/nginx/${id}`)
-        .catch((error: AxiosError<{message: string}>) => 
-            alert(`Something went wrong while fetching rule (id: ${id}).\n${error.response?.data.message}`)
-        )
+        const res = await axios.get<NginxRule>(`https://api.mcsynergy.nl/admin-panel/nginx/${id}`, {
+            headers: {Authorization: `Bearer ${getAuth().currentUser?.getIdToken(true)}`}
+        })
+        .catch((error: AxiosError<{message: string}>) => {
+            if(error.response?.data.message === "failed to get claims") window.location.replace("https://mcsynergy.nl/login")
+            else alert(`Something went wrong while fetching rule (id: ${id}).\n${error.response?.data.message}`)
+        })
         if(!res) return;
         const {data} = res;
         const index = rules.value.findIndex((rule) => rule.id === data.id)
@@ -35,26 +42,35 @@ export const useNginxStore = defineStore('nginx', () => {
     }
 
     const createRule = async (rule: NginxRule) => {
-        await axios.post(`https://api.mcsynergy.nl/admin-panel/nginx`, rule)
-        .catch((error: AxiosError<{message: string}>) => 
-            alert(`Something went wrong while creating rule.\n${error.response?.data.message}`)
-        )
+        await axios.post(`https://api.mcsynergy.nl/admin-panel/nginx`, rule, {
+            headers: {Authorization: `Bearer ${getAuth().currentUser?.getIdToken(true)}`}
+        })
+        .catch((error: AxiosError<{message: string}>) => {
+            if(error.response?.data.message === "failed to get claims") window.location.replace("https://mcsynergy.nl/login")
+            else alert(`Something went wrong while creating rule.\n${error.response?.data.message}`)
+        })
         await fetchAllRules()
     }
 
     const updateRule = async (id: number, rule: NginxRule) => {
-        await axios.patch(`https://api.mcsynergy.nl/admin-panel/nginx/${id}`, rule)
-        .catch((error: AxiosError<{message: string}>) => 
-            alert(`Something went wrong while updating rule (id: ${id}).\n${error.response?.data.message}`)
-        )
+        await axios.patch(`https://api.mcsynergy.nl/admin-panel/nginx/${id}`, rule, {
+            headers: {Authorization: `Bearer ${getAuth().currentUser?.getIdToken(true)}`}
+        })
+        .catch((error: AxiosError<{message: string}>) => {
+            if(error.response?.data.message === "failed to get claims") window.location.replace("https://mcsynergy.nl/login")
+            else alert(`Something went wrong while updating rule (id: ${id}).\n${error.response?.data.message}`)
+        })
         await fetchRuleById(id)
     }
 
     const deleteRule =async (id: number) => {
-        await axios.delete(`https://api.mcsynergy.nl/admin-panel/nginx/${id}`)
-        .catch((error: AxiosError<{message: string}>) => 
-            alert(`Something went wrong while deleting rule (id: ${id}).\n${error.response?.data.message}`)
-        )
+        await axios.delete(`https://api.mcsynergy.nl/admin-panel/nginx/${id}`, {
+            headers: {Authorization: `Bearer ${getAuth().currentUser?.getIdToken(true)}`}
+        })
+        .catch((error: AxiosError<{message: string}>) => {
+            if(error.response?.data.message === "failed to get claims") window.location.replace("https://mcsynergy.nl/login")
+            else alert(`Something went wrong while deleting rule (id: ${id}).\n${error.response?.data.message}`)
+        })
         await fetchAllRules()
     }
 
